b5ea3647302d212455325a0d58c6bb1e3616497f
Paulo Almeida <paulocomy1408@gmail.com>
2022-05-25 20:42:02 -0400

    projeto finalizado

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 const sizeSlider = document.querySelector('#sizeSlider')
 const sliderInfo = document.querySelector('.slider-info')
 const grid = document.querySelector('#grid')
-
 const color = document.querySelector('.color')
 const eraser = document.querySelector('.eraser')
 const clear = document.querySelector('.clear')
 const rainbow = document.querySelector('.rainbow')
+const colorPicker = document.querySelector('.color-picker')
+
+let currentColor = '#333'
+let currentMode = 'color'
+
+const setColor = () => {
+  currentColor = colorPicker.value
+}
+
+const setMode = (e) => {
+  currentMode = e.target.classList[0]
+  setActive(e)
+}
+
+const setActive = (btn) => {
+  color.classList.remove('active')
+  eraser.classList.remove('active')
+  rainbow.classList.remove('active')
+
+  btn.target.classList.add('active')
+}
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-let mode = 'color'
-let setMode = (e) => {
-  mode = e.target.className
-}
 
-let setupGrid = () => {
+const setupGrid = () => {
   clearGrid()
   size(sizeSlider.value)
 
@@ -32,27 +48,52 @@ let setupGrid = () => {
   }
 }
 
-let size = (size) => {
+const size = (size) => {
   sliderInfo.innerText = `${size} X ${size}`
 }
 
-let clearGrid = () => {
+const clearGrid = () => {
   grid.innerHTML = ''
 }
 
-let reloadGrid = () => {
+const reloadGrid = () => {
   setupGrid()
 }
 
-let changeColor = (e) => {
+const changeColor = (e) => {
   e.preventDefault();
   if(e.type === 'mouseover' && !mouseDown) return
-  if(mode == 'color') {
-    e.target.style.background = '#333'
-  }
-  else if(mode == 'eraser') {
+  if(currentMode === 'color') {
+    e.target.style.background = currentColor
+  } else if(currentMode === 'eraser') {
     e.target.style.background = '#fff'
+  } else if(currentMode === 'rainbow') {
+    e.target.style.background = random()
   }
+}
+
+const rgbToHex = rgb => { 
+  let hex = Number(rgb).toString(16) //retorna uma string com a base específicada
+  if (hex.length < 2) {
+    hex = "0" + hex
+  }
+  return hex
+}
+
+//randomiza número, Math.floor remove casa decimais e Math.random randomiza números entre 0 e 255
+const randomize = () => {
+  let number = Math.floor(Math.random() * 256)
+  return number
+}
+
+const random = () => {
+  let red = randomize()
+  let green = randomize()
+  let blue = randomize()
+  red = rgbToHex(red)
+  green = rgbToHex(green)
+  blue = rgbToHex(blue)
+  return '#'+red+green+blue
 }
 
 sizeSlider.addEventListener('input', setupGrid)
@@ -61,5 +102,6 @@ color.addEventListener('click', setMode)
 eraser.addEventListener('click', setMode)
 clear.addEventListener('click', reloadGrid)
 rainbow.addEventListener('click', setMode)
+colorPicker.addEventListener('input', setColor)
 
 setupGrid()
